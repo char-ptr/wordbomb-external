e91c89c5ee8e89757500e1630d139f95eab00aae
amelia <44528100+pozm@users.noreply.github.com>
2024-04-19 10:33:50 +0100

    fix terrible build script

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
+use path_absolutize::Absolutize;
+
 fn main() {
+    let this_path = std::env::current_dir().expect("unable to get cwd??");
+    let absolute = this_path.absolutize().expect("unable to absolutize");
+    let strab = absolute.to_str().expect("unable to get as str");
+
     println!("cargo:rustc-link-lib=dylib=FakerInputDll");
-    println!(r"cargo:rustc-link-search=native=X:\meta\dev\rust\cumin-cli");
+    println!(r"cargo:rustc-link-search=native={strab}");
 }
+
--- src/faker_input/mod.rs
+++ src/faker_input/mod.rs
@@ -2,20 +2,39 @@ pub mod keyboard_report;
 pub mod mouse_report;
 use std::ffi::c_void;
 
-use self::{keyboard_report::KBDReport, mouse_report::{MouseReport}};
+use self::{keyboard_report::KBDReport, mouse_report::MouseReport};
 
 type HNDL = *mut c_void;
 
-#[link(name = "FakerInputDll")]
+#[link(name = "FakerInputDll", kind = "dylib")]
 extern "C" {
     pub fn fakerinput_alloc() -> *mut c_void;
     pub fn fakerinput_free(vmulti: HNDL) -> c_void;
     pub fn fakerinput_connect(vmulti: HNDL) -> bool;
     pub fn fakerinput_disconnect(vmulti: HNDL) -> bool;
-    pub fn fakerinput_update_keyboard(vmulti: HNDL, shiftKeyFlags: u8, keyCodes: *const u8) -> bool;
-    pub fn fakerinput_update_keyboard_enhanced(vmulti: HNDL, mediaKeys: u8, enhancedKeys: u8) -> bool;
-    pub fn fakerinput_update_relative_mouse(vmulti: HNDL, button: u8, x:i16, y:i16, wheelPosition:u8, hWheelPosition:u8) -> bool;
-    pub fn fakerinput_update_absolute_mouse(vmulti: HNDL, button: u8, x:u16, y:u16, wheelPosition:u8, hWheelPosition:u8) -> bool;
+    pub fn fakerinput_update_keyboard(vmulti: HNDL, shiftKeyFlags: u8, keyCodes: *const u8)
+        -> bool;
+    pub fn fakerinput_update_keyboard_enhanced(
+        vmulti: HNDL,
+        mediaKeys: u8,
+        enhancedKeys: u8,
+    ) -> bool;
+    pub fn fakerinput_update_relative_mouse(
+        vmulti: HNDL,
+        button: u8,
+        x: i16,
+        y: i16,
+        wheelPosition: u8,
+        hWheelPosition: u8,
+    ) -> bool;
+    pub fn fakerinput_update_absolute_mouse(
+        vmulti: HNDL,
+        button: u8,
+        x: u16,
+        y: u16,
+        wheelPosition: u8,
+        hWheelPosition: u8,
+    ) -> bool;
 }
 
 pub struct FakerInput {
@@ -53,7 +72,13 @@ impl FakerInput {
             return false;
         }
         let codes = report.get_raw_key_codes();
-        unsafe { fakerinput_update_keyboard(self.vmulti, report.get_raw_shift_key_flags(), codes.as_ptr()) }
+        unsafe {
+            fakerinput_update_keyboard(
+                self.vmulti,
+                report.get_raw_shift_key_flags(),
+                codes.as_ptr(),
+            )
+        }
     }
 
     // pub fn update_keyboard_enhanced(&mut self, mediaKeys: u8, enhancedKeys: u8) -> bool {
@@ -67,11 +92,20 @@ impl FakerInput {
         if !self.connected {
             return false;
         }
-        unsafe { fakerinput_update_relative_mouse(self.vmulti, report.buttons, report.x , report.y , report.wheel, report.h_wheel) }
+        unsafe {
+            fakerinput_update_relative_mouse(
+                self.vmulti,
+                report.buttons,
+                report.x,
+                report.y,
+                report.wheel,
+                report.h_wheel,
+            )
+        }
     }
 
     pub fn update_absolute_mouse(&mut self, report: &MouseReport) -> bool {
-        use mouse_position::mouse_position::{Mouse};
+        use mouse_position::mouse_position::Mouse;
         // if !self.connected {
         //     return false;
         // }
@@ -79,7 +113,7 @@ impl FakerInput {
         // unsafe { fakerinput_update_absolute_mouse(self.vmulti, report.buttons, report.x , report.y , report.wheel, report.h_wheel) }
 
         // it's broken so we'll just use relative mouse for now
-        let current_position  = Mouse::get_mouse_position();
+        let current_position = Mouse::get_mouse_position();
         if let Mouse::Position { x, y } = current_position {
             let mut new_report = report.clone();
             //convert to relative
@@ -87,10 +121,9 @@ impl FakerInput {
             new_report.y = (report.y as i32 - y as i32) as i16;
             println!(" {report:?} -> {:?} [ {x}, {y} ]", new_report);
             self.update_relative_mouse(&new_report)
-        } else { 
+        } else {
             false
         }
-
     }
 }
 
@@ -100,3 +133,4 @@ impl Drop for FakerInput {
         unsafe { fakerinput_free(self.vmulti) };
     }
 }
+
